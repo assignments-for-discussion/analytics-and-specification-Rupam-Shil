18d5aabfc30f49245a51875223d9e6063bba634e
Rupam Shil <rupamshil111@gmail.com>
2021-12-15 19:49:35 +0530

    variance check with new test cases added

--- average.js
+++ average.js
@@ -1,13 +1,41 @@
+/**
+ * @param {array} numbers ;
+ * @param {number} maxVariance  difference between max and min : default 2;
+ * @param {number} maxEachVariance value of high variance in successive /
+ *  reading default 10;
+ * @param {number} highVarianceOccurrence no of occurrence of high variance /
+ *  in successive readings : default 0 ;
+ * @returns average of the giver array if no violation else Nan ;
+ */
 
-function average(numbers) {
+function average(numbers, maxVariance = 0, maxEachVariance = 0,
+    highVarianceOccurrence = 0) {
   let realLength = numbers.length;
-  return (
-    numbers.reduce((p, c) => {
-      if (c) return p + c;
-      realLength--;
-      return p;
-    }, 0) / realLength
-  );
+  const avg = numbers.reduce((p, c) => {
+    if (!isNaN(c)) return p + c;
+    realLength--;
+    return p;
+  }, 0) / realLength;
+
+  let isExtremeVariance = false;
+  const nonNanNumbers = numbers.filter((number) => !isNaN(number));
+  // For maximum variance
+  if (maxVariance) {
+    const min = Math.min(...nonNanNumbers);
+    const max = Math.max(...nonNanNumbers);
+    (max-min) > maxVariance ? isExtremeVariance = true : null;
+  }
+  // For each variance
+  if (maxEachVariance) {
+    for (let i = 0; i < nonNanNumbers.length-1; i++) {
+      if (Math.abs(nonNanNumbers[i]-nonNanNumbers[i+1]) > maxEachVariance) {
+        highVarianceOccurrence--;
+      }
+    }
+    highVarianceOccurrence < 0 ? isExtremeVariance = true : null;
+  }
+  // return the average
+  return !isExtremeVariance ? avg : NaN;
 }
 
 module.exports = {average};
--- test/average.test.js
+++ test/average.test.js
@@ -1,16 +1,32 @@
 const {expect} = require('chai');
 const {average} = require('../average');
 
-it('computes average of a list of numbers', ()=> {
+it('computes average of a list of numbers', () => {
   // floating point numbers cannot be compared for equality,
   // hence allowing a delta tolerance
   expect(average([1, 2, 3, 4])).to.be.approximately(2.5, 0.01);
 });
 
-it('reports the average as NaN on an empty list', ()=> {
+it('reports the average as NaN on an empty list', () => {
   expect(average([])).to.be.NaN;
 });
 
-it('ignores NaN in the input', ()=> {
+it('ignores NaN in the input', () => {
   expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
+});
+
+it('reports the average as NaN on high variance - for a difference in min and max', () => {
+  expect(average([-1, 6, 1, 10], 5)).to.be.NaN;
+});
+
+it('reports the average as NaN on high variance - for each variance', () => {
+  expect(average([0, 4, 5, 13], 0, 2)).to.be.NaN;
+});
+
+it('reports the average as NaN on high variance -1) for each variance with no of occurrence', () => {
+  expect(average([0, 4, 5, 13], 0, 2, 1)).to.be.NaN;
+});
+
+it('reports the average as NaN on high variance -2) for each variance with no of occurrence', () => {
+  expect(average([0, 4, 5, 13], 0, 2, 2)).to.be.approximately(5.5, 0.01);
 });
